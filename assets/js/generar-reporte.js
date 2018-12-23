7b842d7134f856216b3188960ee3ce1284bfcfec
Miguel Angel Quezada <miguel.quezadag@alumno.udg.mx>
2018-12-23 13:30:09 -0600

    Servicio reporte y mejoras de validacion reporte

--- assets/js/generar-reporte.js
+++ assets/js/generar-reporte.js
@@ -1,9 +1,61 @@
 const nuevoReporte = function(){
-  //validar();
-  alert("Boton Generar Reporte");
+  let recibe = document.getElementById('recibe').value;
+  let correo = document.getElementById('correo').value;
+  let telefono = document.getElementById('telefono').value;
+  let area = document.getElementById('area').value;
+  let modulo = $("#modulo option:selected").text();
+  let piso = $("#piso option:selected").text();
+  let aula = $("#aula option:selected").text();
+  let option = document.querySelector('input[name="descripcionServicio"]:checked').value;
+  let descripcionProblema = $("textarea#descripcionProblema").val();
+  //Si se selecciona la opcion otro
+  if (option == 'otro') {
+     option = document.getElementById('otro').value;
+  }
+  registrarReporte(recibe,correo,telefono,area,modulo,piso,aula,option,descripcionProblema);
 }
+/*
 const radOtro = function(){
-  $("#inputOtro").append('<input class="form-control" id="descripcionServicio" type="text" class="validate" name="descripcionServicio" placeholder="Describa el servicio" required>');  
+  $("#inputOtro").html('<input class="form-control" id="descripcionServicio" type="text" class="validate" name="descripcionServicio" placeholder="Describa el servicio" required>').fadeIn();  
+}
+*/
+const registrarReporte = function(recibe,correo,telefono,area,modulo,piso,aula,option,descripcionProblema){
+  console.log(recibe);
+  console.log(correo);
+  console.log(telefono);
+  console.log(area);
+  console.log(modulo);
+  console.log(piso);
+  console.log(aula);
+  console.log(option);
+  console.log(descripcionProblema);
+  var datos = {
+    "recibe" : recibe,
+    "correo" : correo,
+    "telefono" : telefono,
+    "area" : area,
+    "modulo" : modulo,
+    "piso" : piso,
+    "aula" : aula,
+    "option" : option,
+    "descripcionProblema": descripcionProblema
+  }
+  console.log(JSON.stringify(datos));
+  $.ajax({
+    type: 'post',
+    url: 'http://localhost/API-CUCEI-SRG/index.php/reporte/nuevo',
+    data: JSON.stringify(datos),
+    contentType: 'application/json; charset=utf-8',
+    dataType: 'json',
+    success: function(data){
+      console.log("Se ha registrado el reporte exitosamente");
+      console.log(JSON.stringify(data));
+      alert("Registro completado con el folio: "+data.folio)   
+    },
+    error: function(data) {
+      alert('Ha ocurrido un error al intentar registrarse: '+data.mensaje);
+    }
+  });
 }
 const ereaseItems = function(){
     document.getElementById("modulo").disabled = false;
@@ -12,23 +64,29 @@ const ereaseItems = function(){
     $("#modulo").empty();
     $("#piso").empty();
     $("#aula").empty();
-  }
+    $("#modulo").append('<option value="" disabled selected>Seleccione un Módulo.</option>');
+    $("#piso").append('<option value="" disabled selected>Seleccione un Piso.</option>');
+    $("#aula").append('<option value="" disabled selected>Seleccione un Aula.</option>');
+}
   const ereaseModule = function(){
     ereaseFloor();
     ereaseAula();
     document.getElementById("modulo").disabled = false;
     $("#modulo").empty();
-  }
+    $("#modulo").append('<option value="" disabled selected>Seleccione un Módulo.</option>');
+}
   const ereaseFloor = function(){
     ereaseAula();
     document.getElementById("piso").disabled = false;
     $("#piso").empty();
+    $("#piso").append('<option value="" disabled selected>Seleccione un Piso.</option>');
 
-  }
+}
   const ereaseAula = function(){
     document.getElementById("aula").disabled = false;   
     $("#aula").empty();
-  }
+    $("#aula").append('<option value="" disabled selected>Seleccione un Aula.</option>');
+}
   const getModulo = function(){
     $.ajax({
     type: "GET",
@@ -79,29 +137,5 @@ const getAula = function(){
     error: function(data) {
       alert('Debe seleccionar un módulo o piso primero.');
     }
-  });
-}
-const validar = function(){
-  $('#recibe').blur(function(event) {
-    event.target.checkValidity();
-  }).bind('invalid', function(event) {
-    setTimeout(function() { $(event.target).focus();}, 50);
-  });
-
-  $('#correo').blur(function(event) {
-    event.target.checkValidity();
-  }).bind('invalid', function(event) {
-  });
-  
-  $('#telefono').blur(function(event) {
-    event.target.checkValidity();
-  }).bind('invalid', function(event) {
-    setTimeout(function() { $(event.target).focus();}, 50);
-  });
-
-  $('#area').blur(function(event) {
-    event.target.checkValidity();
-  }).bind('invalid', function(event) {
-    setTimeout(function() { $(event.target).focus();}, 50);
   });
 }
--- generar-reporte.php
+++ generar-reporte.php
@@ -26,18 +26,18 @@
                     <input class="form-control" id="recibe" type="text" name="recibe" required>
                     <label for="recibe">Recibe</label>
                 </div>
-                <div class="form-group col-sm-3">
-                  <input class="form-control" id="correo" type="email" class="validate" name="correo" placeholder="email@dominio.udg.mx"  ng-model="correo" required>
+                <div class="col-sm-3">
+                  <input class="form-control" id="correo" type="email" class="validate" name="correo" placeholder="email@dominio.udg.mx" ng-model="correo" required>
                   <span style="color: crimson;" ng-show="formulario.correo.$touched && formulario.correo.$invalid">Email es requerido.<br/></span>
                   <label for="correo">Correo</label>
                 </div>
                 <div class="col-sm-2">
-                 <input type="tel" class="form-control" id="telefono" name="telefono" maxlength="10" minlength="10" pattern="[0-9]{10}" ng-model="telefono" required>
+                 <input type="tel" class="form-control" id="telefono" name="telefono" maxlength="10" minlength="10" pattern="[0-9]{10}" ng-model="telefono" placeholder="3312345678" required>
                  <span style="color: crimson;" ng-show="formulario.telefono.$touched && formulario.telefono.$invalid">Telefono es requerido.<br/></span>
                  <label for="telefono">Teléfono de contacto</label>
                 </div>
                 <div class="input-field col-sm-3">
-                  <input class="form-control" id="area" type="text" class="validate" name="area" ng-model="area" required>
+                  <input class="form-control" id="area" type="text" class="validate" name="area" ng-model="area" placeholder="Area" required>
                   <span style="color: crimson;" ng-show="formulario.area.$touched && formulario.area.$invalid">Area es requerido.<br/></span>
                   <label for="area">Area solicitante</label>
                 </div>
@@ -45,18 +45,18 @@
                   <fieldset>
                     <legend>Ubicacion del servicio</legend>
                     <label for="modulo">Modulo:</label>
-                    <select class="form-control" id="modulo" onclick="getModulo()"></select>
-                    <a href="#" onclick="ereaseModule()">Cambiar</a><br>
+                    <select class="form-control" id="modulo" onclick="getModulo()"><option value="" disabled selected>Seleccione un Módulo.</option></select>
+                    <button class="btn btn-danger" onclick="ereaseModule()">Cambiar</button><br>
                     <label for="piso">Piso:</label>
                     <select class="form-control" id="piso" 
-                    onclick="getPiso()"></select>
-                    <a href="#" onclick="ereaseFloor()">Cambiar</a><br>
+                    onclick="getPiso()"><option value="" disabled selected>Seleccione un Piso.</option></select>
+                    <button class="btn btn-danger" onclick="ereaseFloor()">Cambiar</button><br>
                     <label for="aula">Aula:</label>
                     <select class="form-control" id="aula"
                     onclick="getAula()" 
-                    ></select>
-                    <a href="#" onclick="ereaseAula()">Cambiar</a><br>
-                    <a href="#" onclick="ereaseItems()">Reiniciar</a>
+                    ><option value="" disabled selected>Seleccione un Aula.</option></select>
+                    <button class="btn btn-danger" onclick="ereaseAula()">Cambiar</btn><br>
+                    <button class="btn btn-warning" onclick="ereaseItems()">Reiniciar</button>
                   </fieldset>
                 </div>
                 <div class="input-field col-sm-12">
@@ -126,7 +126,7 @@
                         <div class="input-field col-sm-3">
                             <label>
                                 <span id="inputOtro"></span>
-                                <input name="descripcionServicio" id="descripcionServicio" type="radio" value="11" onclick="radOtro()" />
+                                <input name="descripcionServicio" id="descripcionServicio" type="radio" value="otro"> <input type="text" name="otro" id="otro" placeholder="Otro">
                                 <span>Otro</span>
                             </label>
                         </div>
@@ -135,8 +135,7 @@
                 </div>
                 <div class="input-field col-sm-12" id="txbox">
                   <p>Descripcion del problema</p><br>
-                    <textarea rows="4" cols="50" name="descripcionProblema">
-                    </textarea>
+<textarea rows="4" cols="50" name="descripcionProblema" id="descripcionProblema"></textarea>
                 </div>
                 <div class="col-sm-12" id="divButton">
                   <button class="btn btn-primary" onclick="nuevoReporte()" id="btnNuevoReporte" ng-disabled="formulario.$invalid">Registrar Reporte
