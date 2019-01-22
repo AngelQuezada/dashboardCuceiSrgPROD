00292e37c57cefb2dbd46a78ef272f070897eab0
Miguel Angel Quezada Galvan <miguel.quezadag@alumno.udg.mx>
2019-01-21 19:26:59 -0600

    Se Agrego formularios de seguridad

--- generar-reporte-seguridad-1.php
+++ generar-reporte-seguridad-1.php
@@ -1,14 +1,10 @@
-<?php
-  require('centinela.php');
-?>
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html>
 <head>
-	<title>Nuevo Reporte - Seguridad 1</title>
+	<title>CUCEI-SRG | Administración de Gráficas</title>
 	<?php
     	include('header.php');
-  ?>
- <link rel="stylesheet" type="text/css" href="assets/css/generar-reporte-seguridad-1.css">
+  	?>
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
 	<div class="wrapper">
@@ -19,10 +15,155 @@
       include("sidebar.php");
     ?>
     <div class="content-wrapper">
-      <div class="jumbotron">
-      </div>
+    	<section class="content-header">
+    		<h1>
+		        Nuevo Reporte de Seguridad FORMATO 1
+		        <small>Sistema de Reportes Generales</small>
+      		</h1>
+			<ol class="breadcrumb">
+				<li><a href="#"><i class="fa fa-dashboard"></i> Dashboard-Seguridad</a></li>
+				<li class="active">Nuevo Reporte</li>
+			</ol>
+    	</section>
+    	<!-- END Content Header (Page header) -->
+    	<!-- Contenedor principal -->
     	<section class="content">
-        
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Reporte 1 Seguridad</title>
+</head>
+<body>
+
+    <div style="text-align: center;">
+         <p>UNIVERSIDAD DE GUADALAJARA
+         <P>CENTRO UNIVERITARIO DE CIENCIAS EXACTAS E INGENIERIAS</P>
+         <p>COORDINACION DE SERVICIOS GENERALES</p>
+         <p>UNIDAD DE SEGURIDAD</p>
+         </p>
+         <p><b>Formato 1</b> Hoja de Reporte</p>
+     </div>
+
+      <div class="row" style="margin: 2%">
+        <form action="" method="POST">
+            <div class="col-sm-6">
+              <input class="form-control" id="tipoS" name="tipoS" type="text" class="validate">
+              <label for="escuela">Tipo de servicio</label>
+            </div>
+
+            <div  class="col-sm-2">
+                    <input class="form-control" name="NumeroReporte" id="NumeroReporte" type="text" class="validate">
+                    <label for="fecha">No.Reporte</label>
+            </div>
+
+            <div class="col-sm-6">
+                <input class="form-control" name="afectado" id="afectado" type="text" class="validate">
+                <label for="afectado">Afectado</label>
+            </div>
+
+            <div class="col-sm-3">
+                <input class="form-control" name="edad" id="edad" type="TINYINT" class="validate">
+                <label for="edad">Edad</label>
+            </div>
+
+            <div class="col-sm-3">
+                <input class="form-control" name="carrera" id="carrera" type="text" class="validate">
+                <label for="carrera">Carrera</label>
+            </div>
+
+            <div class="col-sm-3">
+                <input class="form-control" name="codigo" id="codigo" type="text" class="validate">
+                <label for="codigo">Codigo</label>
+            </div>
+
+            <div class="col-sm-3">
+                <input class="form-control" name="telefono" id="telefono" type="text" class="validate">
+                <label for="telefono">Teléfono</label>
+            </div>
+<div class="col-sm-12">
+  <legend style="text-align: center;">REPORTE</legend>
+</div>
+<div class="col-sm-5">
+            <fieldset>
+
+
+              <div class="col-sm-4">
+                <input class="form-control" name="fecha" id="fecha" type="date" class="datepicker">
+                <label for="fecha">Fecha</label>
+              </div>
+
+              <div class="col-sm-3">
+                <input class="form-control" name="tiempo" id="tiempo" type="time" class="timepicker">
+                <label for="tiempo">Hora</label>
+            </div>
+            </fieldset>
+</div>
+            <div class="col-sm-6">
+                <input class="form-control" name="lugar" id="lugar" type="text" class="validate">
+                <label for="lugar">Lugar</label>
+            </div>
+
+            <div class="input-field col-sm-12" style="text-align:center">
+                <p>Hechos</p><br>
+                <textarea rows="4" cols="50">
+                </textarea>
+            </div>
+          <!--  <fieldset>-->
+          <div class="col-sm-12">
+          <legend style="text-align: center;">DESCRIPCION DE OBJETOS</legend>
+        </div>
+          <div class="col-sm-12">
+            <div class="col-sm-3">
+            <input class="form-control" name="modelo" id="modelo" type="text" class="validate">
+            <label for="modelo">Modelo</label>
+            </div>
+
+            <div class="col-sm-3">
+            <input class="form-control" name="marca" id="marca" type="text" class="validate">
+            <label for="marca">Marca</label>
+            </div>
+
+            <div class="col-sm-3">
+            <input class="form-control" name="tipo" id="tipo" type="text" class="validate">
+            <label for="tipo">Tipo</label>
+            </div>
+
+            <div class="col-sm-3">
+            <input class="form-control" name="año" id="año" type="date" class="validate">
+            <label for="año">Año</label>
+            </div>
+
+            <div class="col-sm-3">
+            <input class="form-control" name="color" id="color" type="text" class="validate">
+            <label for="color">Color</label>
+            </div>
+
+            <div class="col-sm-3">
+            <input class="form-control" name="rodado" id="rodado" type="text" class="validate">
+            <label for="rodado">Rodado</label>
+            </div>
+          </div>
+          <!--</fieldset>-->
+            <div class="col-sm-12" style="text-align: center;"><button class="btn waves-effect waves-light" type="submit" name="action" id="btn-reset" style="background-color: rgb(62, 88, 233);">Registrar Reporte</button>
+            </div>
+    </form>
+</div>
+</body>
+<script type="text/javascript">
+    $(document).ready(function(){
+    $('.datepicker').datepicker();
+  });
+</script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</html>
+
     	</section>
     </div>
     <?php
--- generar-reporte-seguridad-2.php
+++ generar-reporte-seguridad-2.php
@@ -1,11 +1,10 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html>
 <head>
-	<title>Nuevo Reporte - Seguridad 2</title>
+	<title>CUCEI-SRG | Administración de Gráficas</title>
 	<?php
     	include('header.php');
-  ?>
- <link rel="stylesheet" type="text/css" href="assets/css/generar-reporte-seguridad-2.css">
+  	?>
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
 	<div class="wrapper">
@@ -16,10 +15,201 @@
       include("sidebar.php");
     ?>
     <div class="content-wrapper">
-      <div class="jumbotron">
-      </div>
+    	<section class="content-header">
+    		<h1>
+		        Nuevo Reporte de Seguridad FORMATO 2
+		        <small>Sistema de Reportes Generales</small>
+      		</h1>
+			<ol class="breadcrumb">
+				<li><a href="#"><i class="fa fa-dashboard"></i> Dashboard-Seguridad</a></li>
+				<li class="active">Nuevo Reporte</li>
+			</ol>
+    	</section>
+    	<!-- END Content Header (Page header) -->
+    	<!-- Contenedor principal -->
     	<section class="content">
+      <div style="text-align: center;">
+         <p>UNIVERSIDAD DE GUADALAJARA 
+         <P>COORDINACION DE SEGURIDAD UNIVERSITARIA</P> 
+         </p>
+         <p><b>Formato 2</b> Reporte de incidencias</p>
+     </div>
+     
+     <fieldset>
+     <legend style="text-align: center;">DATOS DE IDENTIFICACION DEL AFECTADO</legend>
 
+      <div class="row" style="margin: 2%">
+          <form action="" method="POST">
+            <div class="col-sm-3">
+              <input class="form-control" id="escuela" name="escuela" type="text" class="validate">
+              <label for="escuela">escuela/institucion</label>
+            </div>
+            
+            <div class="col-sm-3">
+                    <input class="form-control" name="fecha" id="fecha" type="date" class="datepicker">
+                    <label for="fecha">Fecha</label>
+                  </div>
+                  
+                  <div class="col-sm-3">
+                        <input class="form-control" name="nombre" id="nombre" type="text" class="validate">
+                        <label for="nombre">Nombre</label>
+                      </div>
+
+                  <div class="col-sm-1">
+                       <input class="form-control" name="edad" id="edad" type="TINYINT" class="validate">
+                       <label for="edad">Edad</label>
+                     </div>
+
+                   <div class="col-sm-3">
+                       <input class="form-control" name="codigo" id="codigo" type="text" class="validate">
+                       <label for="codigo">Codigo</label>
+                     </div>
+
+                     <div class="col-sm-3">
+                       <input class="form-control" name="carrera" id="carrera" type="text" class="validate">
+                       <label for="carrera">Carrera</label>
+                     </div>
+
+                     <div class="col-sm-3">
+                       <input class="form-control" name="email" id="email" type="email" class="validate">
+                       <label for="email">Email</label>
+                     </div>
+
+                     <div class="col-sm-3">
+                      <input class="form-control" name="telefono" id="telefono" type="tel" class="validate">
+                      <label for="telefono">Teléfono de contacto</label>
+                    </div>
+                  </fieldset>
+                    
+          
+  
+                <fieldset>
+                    <legend style="text-align: center;">DATOS DEL INCIDENTE</legend>
+                    <div class="col-sm-3">
+                      <input class="form-control" name="fecha" id="fecha" type="date" class="datepicker">
+                      <label for="fecha">Fecha</label>
+                     </div>
+
+                    <div class="col-sm-3">
+                     <input class="form-control" name="tiempo" id="tiempo" type="time" class="timepicker">
+                     <label for="tiempo">Hora</label>
+                    </div>
+                 
+                    <div class="col-sm-4">
+                     <input class="form-control" name="lugar" id="lugar" type="text" class="validate">
+                     <label for="lugar">Lugar (calle y cruces) Frente a (casa,oficina,baldios,edificios,etc)</label>
+                    </div>
+                  </fieldset>
+                  
+                  <fieldset>
+                    <div class="input-field col-sm-6" style="text-align:center">
+                        <legend>Descripcion el suceso</legend><br>
+                          <textarea rows="4" cols="50">
+                          </textarea>
+                      </div>
+
+                      <div class="input-field col-sm-6" style="text-align:center">
+                            <legend>Descripcion de lo robado</legend><br>
+                              <textarea rows="4" cols="50">
+                              </textarea>
+                      </div>
+                   </fieldset>
+                   
+            <fieldset>
+            <legend style="text-align: center;">MEDIDA FILIACION DEL AGRESOR</legend>
+
+                  <div class="col-sm-3">
+                    <input class="form-control" name="estatura" id="estatura" type="text" class="validate">
+                    <label for="estatura">Estatura</label>
+                   </div>
+
+                    <div class="col-sm-3">
+                      <input class="form-control" name="apariencia" id="apariencia" type="text" class="validate">
+                      <label for="apariencia">Apariencia</label>
+                     </div>
+
+                     <div class="col-sm-3">
+                       <input class="form-control" name="tez" id="tez" type="text" class="validate">
+                       <label for="tez">Tez</label>
+                      </div>
+
+                      <div class="col-sm-3">
+                        <input class="form-control" name="cabello" id="cabello" type="text" class="validate">
+                        <label for="cabello">Cabello</label>
+                      </div>
+
+                      <div class="col-sm-3">
+                        <input class="form-control" name="ojos" id="ojos" type="text" class="validate">
+                        <label for="ojos">Ojos</label>
+                      </div>
+
+                      <div class="col-sm-3">
+                        <input class="form-control" name="cara" id="cara" type="text" class="validate">
+                        <label for="cara">Cara</label>
+                      </div>
+
+                      <div class="col-sm-3">
+                        <input class="form-control" name="boca" id="boca" type="text" class="validate">
+                        <label for="boca">Boca</label>
+                      </div>
+
+                      <div class="col-sm-3">
+                        <input class="form-control" name="ropa" id="ropa" type="text" class="validate">
+                        <label for="ropa">Tipo de ropa</label>
+                      </div>
+
+                      <div class="col-sm-3">
+                         <input class="form-control" name="uso" id="uso" type="text" class="validate">
+                         <label for="uso">Uso,gorra</label>
+                       </div>
+
+                      <div class="col-sm-3">
+                        <input class="form-control" name="edad" id="edad" type="TINYINT" class="validate">
+                        <label for="edad">Edad Aprox</label>
+                      </div>
+
+                       <div class="col-sm-3">
+                        <input class="form-control" name="cicatrices" id="cicatrices" type="text" class="validate">
+                        <label for="cicatrices">Cicatrices</label>
+                       </div>
+
+                       <div class="col-sm-3">
+                         <input class="form-control" name="tatuajes" id="tatuajes" type="text" class="validate">
+                         <label for="tatuajes">Tatuajes</label>
+                       </div>
+                                                             
+                       <div class="col-sm-3">
+                        <input class="form-control" name="percing" id="percing" type="text" class="validate">
+                        <label for="percing">Percing</label>
+                       </div>
+
+                       <div class="col-sm-3">
+                         <input class="form-control" name="otras" id="otras" type="text" class="validate">
+                         <label for="otras">Otra seña particular</label>
+                        </div>
+                      </fieldset>
+                      
+                      <fieldset>
+                        <div style="text-align: center;" class="col-sm-50">
+                        <input class="form-control" name="caracteristicasvehiculo" id="caracteristicasvehiculo" type="text" class="validate">
+                        <label for="caracteristicasvehiculo">Medio utilizado para huir (caracteristicas del vehiculo en su caso)</label>
+                       </div>
+                        
+                       <div class="col-sm-12">
+                        <p>Observaciones</p>
+                      <textarea rows="4" cols="150">
+                          </textarea>
+                       </div>
+
+                      </fieldset>
+                       <div style="text-align: center;">
+
+  <button class="btn waves-effect waves-light" type="submit" name="action" id="btn-reset" style="background-color: rgb(62, 88, 233);">Registrar Reporte
+           </button>
+         </div>
+        </div>
+        </form>
+      </div>
     	</section>
     </div>
     <?php
